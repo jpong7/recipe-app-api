38ee247016d24ea900190be45784da75fe510e05
Bovornpot <melodyharmonyy@gmail.com>
2021-04-06 15:40:57 +0700

    end 45.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -6,7 +6,6 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
-TOKEN_URL = reverse('user:token')
 
 
 def create_user(**params):
